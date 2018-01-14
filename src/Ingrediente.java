64bfeb5cc5331f7dd4a1a3b48b70fb69ee24cdc2
henriquesuenaga88 <henriquesuenaga@gmail.com>
2018-01-14 11:57:08 -0200

    Adicionando quantidade ao ingrediente

--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -2,7 +2,14 @@ public class Ingrediente {
 
     public String nome;
 
+    public Integer quantidade;
+
     public Ingrediente(String nome) {
         this.nome = nome;
+    }
+
+    public Ingrediente(String nome, Integer quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
     }
 }
